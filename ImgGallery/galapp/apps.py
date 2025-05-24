55cceb5650bf9f1e578cdcd04d04b8231e41ec58
Darkknight1213 <riyazahamedff@gmail.com>
2025-05-24 21:38:10 +0530

    success

--- ImgGallery/galapp/apps.py
+++ ImgGallery/galapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ImgConfig(AppConfig):
+class GalappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Img'
+    name = 'galapp'
